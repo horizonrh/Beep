d10188d4520654dc5da2dc578847cca9d25b692c
Aska <songv.liu@gmail.com>
2015-08-31 22:38:39 +0800

    init

---
+++ Beep/Beep/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Beep
+//
+//  Created by cdxahz on 15/8/31.
+//  Copyright (c) 2015年 CDXAHZ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
